5b435c77bc771820ea5a8069ed94ec3355397f9e
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-02-11 13:17:07 -0800

    Simplify bootstrap.sh a bit

--- jwt-authorizer/bootstrap.sh
+++ jwt-authorizer/bootstrap.sh
@@ -16,9 +16,6 @@ fi
 echo "Hostname:"
 read HOSTNAME
 
-echo "Kubernetes Namespace (prefixes for generated files):"
-read NAMESPACE
-
 echo "CILogon client ID for hostname (for callback https://$HOSTNAME/oauth2/callback):"
 read OAUTH2_PROXY_CLIENT_ID
 
@@ -44,17 +41,16 @@ AUTHORIZER_FLASK_SECRET=$(dd if=/dev/urandom bs=32 count=1 2> /dev/null | $b64)
 
 cat <<EOF
 Writing Files:
-${NAMESPACE}-private.pem
-${NAMESPACE}-flask_secret.txt
-${NAMESPACE}-oauth2-proxy-cookie-secret.txt
-${NAMESPACE}-oauth2-proxy-client-secret.txt
+private.pem
+flask_secret.txt
+oauth2-proxy-cookie-secret.txt
+oauth2-proxy-client-secret.txt
 EOF
 
 rm public.pem
-mv private.pem ${NAMESPACE}-private.pem
-echo -n $AUTHORIZER_FLASK_SECRET > ${NAMESPACE}-flask_secret.txt
-echo -n $OAUTH2_PROXY_COOKIE_SECRET > ${NAMESPACE}-oauth2-proxy-cookie-secret.txt
-echo -n $OAUTH2_PROXY_CLIENT_SECRET > ${NAMESPACE}-oauth2-proxy-client-secret.txt
+echo -n $AUTHORIZER_FLASK_SECRET > flask_secret.txt
+echo -n $OAUTH2_PROXY_COOKIE_SECRET > oauth2-proxy-cookie-secret.txt
+echo -n $OAUTH2_PROXY_CLIENT_SECRET > oauth2-proxy-client-secret.txt
 
 cat <<EOF
 You may initialize and modify your values file as follows:
@@ -62,10 +58,10 @@ You may initialize and modify your values file as follows:
 # If you are not using vault, edit the secrets section:
 secrets:
   enabled: True
-  flask_secret: "" # See: ${NAMESPACE}-flask_secret.txt
-  oauth2_proxy_cookie_secret: "" # See: ${NAMESPACE}-oauth2-proxy-cookie-secret.txt
-  oauth2_proxy_client_secret: "" # See: ${NAMESPACE}-oauth2-proxy-client-secret.txt
-  signing_key: "" # See: ${NAMESPACE}-private.pem
+  flask_secret: "" # See: flask_secret.txt
+  oauth2_proxy_cookie_secret: "" # See: oauth2-proxy-cookie-secret.txt
+  oauth2_proxy_client_secret: "" # See: oauth2-proxy-client-secret.txt
+  signing_key: "" # See: private.pem
 
 # Otherwise, edit the vault_secrets as follows:
 vault_secrets:
@@ -82,15 +78,15 @@ oauth2_proxy_client_id: ${OAUTH2_PROXY_CLIENT_ID}
 # export VAULT_ADDR=...
 # export VAULT_TOKEN=...
 vault kv put "secret/k8s_operator/${HOSTNAME}/jwt_authorizer" \\
-    flask_secret.txt=@${NAMESPACE}-flask-secret.txt \\
-    signing_key.pem=@${NAMESPACE}-private.pem \\
-    oauth2-proxy-cookie-secret.txt=@${NAMESPACE}-oauth2-proxy-cookie-secret.txt \\
-    oauth2-proxy-client-secret.txt=@${NAMESPACE}-oauth2-proxy-client-secret.txt
+    flask_secret.txt=@flask-secret.txt \\
+    signing_key.pem=@private.pem \\
+    oauth2-proxy-cookie-secret.txt=@oauth2-proxy-cookie-secret.txt \\
+    oauth2-proxy-client-secret.txt=@oauth2-proxy-client-secret.txt
 
 rm \\
-    ${NAMESPACE}-flask_secret.txt \\
-    ${NAMESPACE}-private.pem \\
-    ${NAMESPACE}-oauth2-proxy-cookie-secret.txt \\
-    ${NAMESPACE}-oauth2-proxy-client-secret.txt
+    flask_secret.txt \\
+    private.pem \\
+    oauth2-proxy-cookie-secret.txt \\
+    oauth2-proxy-client-secret.txt
 
 EOF
